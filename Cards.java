aa1f39841e19ac888fd59aba2cb0f038e6f3868b
Bhavika Adukia <Bhavika.Adukia@cdk.com>
2016-07-07 15:17:46 +0530

    Added getPip()

--- Cards.java
+++ Cards.java
@@ -1,43 +1,55 @@
-class Cards1 {
+class Card {
 	char pip;
 	char suit;
 	int rank;
-	Cards1(char pip, char suit) {
+	char pipArray[] = {'0', 'A','2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
+
+	Card(char pip, char suit) {
 		this.pip = pip;
 		this.suit = suit;
 		rank = 0;
 	}
-	boolean compare(Cards1 card1) {
+	boolean compare(Card c) {
 		int i = 0;
-		char[] piparray =  {'A','2','3','4','5','6','7','8','9','T','J','Q','K','\0'};
 		char[] suitarray = {'C','D','H','S','\0'};
-		for(;piparray[i]!='\0';i++) {
-			if(pip == piparray[i]) {
+		for(;i < pipArray.length-1;i++) {
+			if(pip == pipArray[i]) {
 				rank += i;
 			}
-			if(card1.pip == piparray[i]) {
-				card1.rank +=i;
+			if(c.pip == pipArray[i]) {
+				c.rank +=i;
 			}
 		}
 		for(i = 0;suitarray[i]!='\0';i++) {
 			if(suit == suitarray[i]) {
 				rank += i*10;
 			}
-			if(card1.pip == piparray[i]) {
-				card1.rank +=i*10;
+			if(c.suit == suitarray[i]) {
+				c.rank +=i*10;
 			}
 		}
-		return rank > card1.rank ? true : false;
+		return rank > c.rank ? true : false;
+	}
+	public int getPip() {
+		int pipVal = 0;
+		for (int i = 0; i < pipArray.length-1; i++) {
+			if (pip == pipArray[i]) {
+				pipVal = i;
+			}
+		}
+		return pipVal;
 	}	
 }
 class Cards {
 	public static void main(String[] args){
-		Cards1 card1 = new Cards1('3','D');
-		Cards1 card2 = new Cards1('J','C');
+		Card card1 = new Card('3','D');
+		Card card2 = new Card('J','C');
 		if(card2.compare(card1)) {
 			System.out.println("card2");
 		}else {
 			System.out.println("card1");
 		}
+		System.out.println(card1.getPip());
+		System.out.println(card2.getPip());
 	}
 }
