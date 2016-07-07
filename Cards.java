f8341b5de83a804abfdac85c03e0f0acefcd3327
rajyasri601@gmail.com <Rajyasri Ashok>
2016-07-07 22:08:37 +0530

    some errors fixed

--- Cards.java
+++ Cards.java
@@ -1,23 +1,16 @@
 class Card {
 	char pip;
 	char suit;
-	char[] pipArray = {'0', 'A','2', '3', '4', '5', '6', '7', '8', '9', 'T', 							'J', 'Q', 'K'};
-	char[] suitArray = {'C','D','H','S'};
-
 	Card(char pip, char suit) {
 		this.pip = pip;
 		this.suit = suit;
 	}
-	int compare(Card c1, Card c2) {
-		if (c1.getPip() > c2.getPip()) 
-		return 1;
-		else if (c1.getPip() < c2.getPip())
-		return -1;
-		else
-		return 0;
+	int compare(Card c) {
+		return getPip() - c.getPip(); 
 	}
 	public int getPip() {
 		int pipVal = 0;
+		char[] pipArray = {'0', 'A','2', '3', '4', '5', '6', '7', '8', '9', 							'T', 'J', 'Q', 'K'};
 		for (int i = 0; i < pipArray.length; i++) {
 			if (pip == pipArray[i]) {
 				pipVal = i;
@@ -30,7 +23,6 @@ class Cards {
 	public static void main(String[] args){
 		Card card1 = new Card('3','D');
 		Card card2 = new Card('J','C');
-		
 		System.out.println(card1.getPip());
 		System.out.println(card2.getPip());
 	}
