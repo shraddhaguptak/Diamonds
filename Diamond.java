f4d95c81e0a95a8ad99ff7fdd2bbecd16eec1fbe
Bhavika Adukia <Bhavika.Adukia@cdk.com>
2016-07-08 06:44:17 +0530

    Added randomShuffle() and removeUsedCard()

--- Diamond.java
+++ Diamond.java
@@ -8,6 +8,12 @@ public class Diamond {
 		for (int i = 1; i<= 13; i++) {
 			diamond.add(new Card(i, 'D'));
 		}
+	}public void randomShuffle() {
+		Collections.shuffle(diamond);
+		
+	}
+	public void removeUsedCard(int diamondVal) {
+		diamond.remove(diamondVal);
 	}
 	public static void main(String[] args) {
 		Diamond d = new Diamond();
